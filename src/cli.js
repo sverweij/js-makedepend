777b1ad2ff7575dc6a35c31d88309f409faba5aa
sverweij <sverweij@users.noreply.github.com>
2015-09-24 21:27:58 +0200

    [src] emit help when run without args, make args mandatory

--- src/cli.js
+++ src/cli.js
@@ -7,14 +7,18 @@ program
     .option('-x, --exclude <regex>', 'a regular expression for excluding modules')
     .option('-s, --delimiter <string>', 'starting string delimiter')
     .option('-d, --flat-define <string>', 'outputs a define with flat dependencies')
-    .option('-1, --start-with <file>', 'to start the depency tree with')
+    .arguments('<directory-or-file>')
     .parse(process.argv);
-
-chewy.main(
-    program.args[0], 
-    {
-        exclude: program.exclude,
-        outputTo: program.outputTo,
-        delimiter: program.delimiter
-    }
-);
+    
+if (!!program.args[0]){
+    chewy.main(
+        program.args[0], 
+        {
+            exclude: program.exclude,
+            outputTo: program.outputTo,
+            delimiter: program.delimiter
+        }
+    );
+} else {
+    program.help();
+}
