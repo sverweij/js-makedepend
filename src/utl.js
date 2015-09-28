2b50af661a12b11f3bdc887a90c5a30fd97faa66
sverweij <sverweij@users.noreply.github.com>
2015-09-28 23:55:14 +0200

    [src] memoize often called filesystem acces functions

--- src/utl.js
+++ src/utl.js
@@ -1,15 +1,16 @@
 var fs   = require ("fs");
 var path = require ("path");
+var _    = require ("lodash");
 
-exports.fileExists = function (pFile) {
+exports.fileExists = _.memoize (function (pFile) {
     try {
         fs.accessSync(pFile,fs.R_OK);
     } catch (e){
         return false;
     }
     return true;
-};
+});
 
-exports.getDirectory = function(pDirOrFile) {
+exports.getDirectory = _.memoize(function(pDirOrFile) {
     return fs.statSync(pDirOrFile).isDirectory() ? pDirOrFile : path.dirname(pDirOrFile);
-};
+});
