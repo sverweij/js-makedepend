6a38e64d925534bdd4b969773f54c10eac39ece8
sverweij <sverweij@users.noreply.github.com>
2015-10-03 21:38:41 +0200

    [test] cull unused utl functions

--- test/utl/testutensils.js
+++ test/utl/testutensils.js
@@ -1,5 +1,5 @@
 var assert = require("assert");
-var fs = require('fs');
+var fs     = require('fs');
 var crypto = require('crypto');
 
 var gHashToUse = [ 'ripemd160', 'md5', 'sha1'].filter(function(h){
@@ -12,28 +12,12 @@ module.exports = (function() {
         return crypto.createHash(gHashToUse).update(pString).digest('hex');
     }
     
-return {
-    assertequalJSON : function(pExpected, pFound) {
-        assert.equal(hashit(JSON.stringify(pExpected)), hashit(JSON.stringify(pFound)));
-    },
-
-    assertequalJSONFile : function(pExpectedFileName, pFound) {
-        var lJSONFromFile = fs.readFileSync(pExpectedFileName, {"encoding": "utf8"});
-        assert.equal(hashit(JSON.stringify(pFound)), hashit(JSON.stringify(JSON.parse(lJSONFromFile))));
-    },
-
-    assertFileEqual : function(pActualFileName,pExpectedFileName) {
-        assert.equal(
-            hashit(fs.readFileSync(pActualFileName, {"encoding": "utf8"})), 
-            hashit(fs.readFileSync(pExpectedFileName, {"encoding": "utf8"}))
-        );
-    },
-    
-    assertequalProcessing : function(pExpectedFileName, pInputFileName, pProcessingFn){
-        var lExpectedContents = fs.readFileSync(pExpectedFileName, {"encoding" : "utf8"});
-        var lInputContents = fs.readFileSync(pInputFileName, {"encoding" : "utf8"});
-        assert.equal(hashit(pProcessingFn(lInputContents)), hashit(lExpectedContents));
-    },
-};
-
+    return {
+        assertFileEqual : function(pActualFileName,pExpectedFileName) {
+            assert.equal(
+                hashit(fs.readFileSync(pActualFileName, {"encoding": "utf8"})), 
+                hashit(fs.readFileSync(pExpectedFileName, {"encoding": "utf8"}))
+            );
+        }
+    };
 })();
