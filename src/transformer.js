24690b120ee74992a2df39cae4024aabb9969bd5
sverweij <sverweij@users.noreply.github.com>
2016-05-12 23:37:43 +0200

    [WIP] adds a directory scanner and connects it to chewy for regular recursive scans
    - grouping by module system is mostly absent
    
    - module system comments get repeated
    
    [ci skip]

--- src/transformer.js
+++ src/transformer.js
@@ -3,9 +3,24 @@
 const extractor = require('./extractor');
 const _         = require('lodash');
 const fs        = require('fs');
+const path      = require('path');
+
+let gScanned    = new Set();
 
 let isNonCoreModule = pDep => !pDep.coreModule;
 let hasNonCoreModuleDependencies = (pDeps, pDependor) => pDeps[pDependor].some(isNonCoreModule);
+let notInCache = pFileName => !gScanned.has(pFileName);
+let getAllJSFilesFromDir = pDirName =>
+    fs.readdirSync(pDirName).reduce((pSum, pFileName) => {
+        if (fs.statSync(path.join(pDirName, pFileName)).isDirectory()){
+            return pSum.concat(getAllJSFilesFromDir(path.join(pDirName, pFileName)));
+        }
+        if (pFileName.endsWith(".js")){
+            return pSum.concat(path.join(pDirName, pFileName));
+        }
+        return pSum;
+    }, []);
+
 
 function reduceDependencies(pPrev, pNext) {
     return `${pPrev} \\\n\t${pNext}`;
@@ -31,31 +46,60 @@ function reduceDependorFlat(pDeps, pPrev, pNext) {
 }
 
 function transformRecursive(pFilename, pOptions){
+    if (gScanned.has(pFilename)) {return;}
+
     let lDependencies = extractor.extractRecursive(pFilename, pOptions);
-    return Object.keys(lDependencies)
+    let lRetval = Object.keys(lDependencies)
+            .filter(notInCache)
             .filter(_.curry(hasNonCoreModuleDependencies)(lDependencies))
             .reduce(_.curry(reduceDependor)(lDependencies), "");
+    Object.keys(lDependencies).forEach(lDep => gScanned.add(lDep));
+    return lRetval;
 }
 
 function transformRecursiveFlattened(pFilename, pOptions){
+    if (gScanned.has(pFilename)) {return;}
     let lDependencies = extractor.extractRecursiveFlattened(pFilename, pOptions);
     return Object.keys(lDependencies)
+            .filter(notInCache)
             .filter(_.curry(hasNonCoreModuleDependencies)(lDependencies))
             .reduce(_.curry(reduceDependorFlat)(lDependencies), "");
+}
+
+function doMagic(pDirOrFile, pOptions) {
+    let lOptions = _.clone(pOptions);
+    let lRetval  = "";
+
+    pOptions.moduleSystems.forEach(pModuleSystem => {
+        lOptions.moduleSystems = [pModuleSystem];
+        lRetval +=
+            `# ${pModuleSystem} dependencies\n` +
+            transformRecursive(pDirOrFile, lOptions);
+    });
+    return lRetval;
 }
 
 exports.getDependencyStrings = (pDirOrFile, pOptions) => {
     let lRetval = "";
     let lOptions = _.clone(pOptions);
+
+    gScanned.clear();
+
     if (!pOptions.append){
         lRetval = `\n${pOptions.delimiter}\n\n`;
     }
 
     if (fs.statSync(pDirOrFile).isDirectory()){
         if (pOptions.flatDefine){
-            return lRetval + `# directory scanning yet to be implemented for flat define\n`;
-        } else {
             return lRetval + `# directory scanning yet to be implemented for everything\n`;
+        } else {
+            return getAllJSFilesFromDir(pDirOrFile)
+                    .reduce((pSum, pDirOrFile) => {
+                        if(!gScanned.has(pDirOrFile)) {
+                            return pSum + doMagic(pDirOrFile, pOptions);
+                        }
+                        return pSum;
+                    }, lRetval);
         }
     } else {
         if (pOptions.flatDefine){
@@ -69,13 +113,7 @@ exports.getDependencyStrings = (pDirOrFile, pOptions) => {
             });
             return lRetval;
         } else {
-            pOptions.moduleSystems.forEach(pModuleSystem => {
-                lOptions.moduleSystems = [pModuleSystem];
-                lRetval +=
-                    `# ${pModuleSystem} dependencies\n` +
-                    transformRecursive(pDirOrFile, lOptions);
-            });
-            return lRetval;
+            return doMagic(pDirOrFile, pOptions);
         }
     }
 };
